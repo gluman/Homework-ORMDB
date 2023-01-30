c740ceadfbae01c629d2505181248741a13931c7
Glumov Andrey <gluman@yandex.ru>
2023-01-30 20:51:31 +0300

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-
+# Подключается к БД любого типа на ваш выбор (например, к PostgreSQL). +
+# Импортирует необходимые модели данных. +
+# Принимает имя или идентификатор издателя (publisher), например через input(). Выводит построчно факты покупки книг этого издателя:
 
 import sqlalchemy
 import sqlalchemy as sq
@@ -7,7 +9,7 @@ from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 Base = declarative_base()
 
 
-class Publisher(Base)
+class Publisher(Base):
     __table__ = 'publisher'
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=60), unique=True)
@@ -29,7 +31,60 @@ class Shop(Base):
     name = sq.Column(sq.String(length=60), unique=True)
 
 class Stock(Base):
+    _tablename__ = "stock"
+
     id = sq.Column(sq.Integer, primary_key=True)
     id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
     count = sq.Column(sq.Integer,nullable=False)
+
+class Sale(Base):
+    _tablename__ = "sale"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Integer, nullable=False)
+    date_sale = sq.Column(sq.Date, nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock'), nullable=False)
+    count = sq.Column(sq.Integer,nullable=False)
+
+
+
+def create_tables(engine):
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+DSN = "postgresql://postgres:postgres@localhost:5432/ormdb"
+engine = sqlalchemy.create_engine(DSN)
+create_tables(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def load_data():
+    pass
+
+def show_sales():
+    pass
+
+run = True
+def show_help():
+    print('''
+    load - load data from datasets
+    help - show help
+    sales - show sales
+    exit - exit
+    ''')
+    return
+
+
+
+while run:
+    i = input('Введите команду (h- помощь): ')
+    if i == 'c':
+        load_data()
+    elif i == 'help':
+        show_help()
+    elif i == 'sales':
+        show_sales()
+    elif i == 'exit':
+        run = False
