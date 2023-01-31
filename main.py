3c6a422b2bce6b5605ff259532a7220c48fb441b
Glumov Andrey <gluman@yandex.ru>
2023-01-31 17:56:56 +0300

    commit 2023.01.31 v1

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 # Импортирует необходимые модели данных. +
 # Принимает имя или идентификатор издателя (publisher), например через input(). Выводит построчно факты покупки книг этого издателя:
 import os
+import csv
 import pprint
 import sqlalchemy
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
-pp
 Base = declarative_base()
 
 
@@ -62,25 +62,18 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-
-
-
-
 def load_data():
+    os.chdir('fixtures')
     folder = os.getcwd()
     files = os.listdir()
     for file in files:
-        if 'sales.csv' in file and file != target.name:
+        if 'sales.csv' in file:
             path_to_file = os.path.join(folder, file)
             with open(path_to_file, 'rt', encoding='utf-8') as f:
                 rows = []
-                count = 0
                 for line in f:
                     rows.append(line.rstrip('\n'))
-                    count += 1
-                res.append({'file_name': file,
-                            'count_rows': count,
-                            'rows': rows})
+
             f.close()
 
 def show_sales():
