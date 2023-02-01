33563827ce22c0a88aa11033a4b19b42e81424bb
Glumov Andrey <gluman@yandex.ru>
2023-02-01 22:12:53 +0300

    commit 2023.02.01 v1

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 # Импортирует необходимые модели данных. +
 # Принимает имя или идентификатор издателя (publisher), например через input(). Выводит построчно факты покупки книг этого издателя:
 import os
-import csv
+import pandas as pd
 import pprint
+
 import sqlalchemy
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 Base = declarative_base()
-
 
 class Publisher(Base):
     __table__ = 'publisher'
@@ -26,77 +26,103 @@ class Book(Base):
 
     publisher = relationship(Publisher, backref='book')
 
+
 class Shop(Base):
     __tablename__ = "shop"
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=60), unique=True)
 
+
 class Stock(Base):
-    _tablename__ = "stock"
+    __tablename__ = "stock"
 
     id = sq.Column(sq.Integer, primary_key=True)
     id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
+    shop = relationship(Shop, backref='shop')
+    book = relationship(Book, backref='book')
+
+
 class Sale(Base):
-    _tablename__ = "sale"
+    __tablename__ = "sale"
 
     id = sq.Column(sq.Integer, primary_key=True)
     price = sq.Column(sq.Integer, nullable=False)
     date_sale = sq.Column(sq.Date, nullable=False)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock'), nullable=False)
-    count = sq.Column(sq.Integer,nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+    stock = relationship(Stock, backref='sale')
 
 
-def create_tables(engine):
-    # Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+def create_tables(engine, command):
+    if command == 'create':
+        Base.metadata.create_all(engine)
+    elif command == 'drop':
+        Base.metadata.drop_all(engine)
 
-DSN = "postgresql://postgres:postgres@localhost:5432/ormdb"
+
+# DSN = "postgresql://postgres:postgres@localhost:5432/ormdb"
+DSN = 'jdbc://postgres:postgres@localhost:5432/ormdb'
 engine = sqlalchemy.create_engine(DSN)
-create_tables(engine)
-
+create_tables(engine, 'create')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-
 def load_data():
     os.chdir('fixtures')
-    folder = os.getcwd()
-    files = os.listdir()
-    for file in files:
-        if 'sales.csv' in file:
-            path_to_file = os.path.join(folder, file)
-            with open(path_to_file, 'rt', encoding='utf-8') as f:
-                rows = []
-                for line in f:
-                    rows.append(line.rstrip('\n'))
+    # folder = os.getcwd()
 
-            f.close()
+    publisher_xl = pd.read_excel('Dataset.xlsx', sheet_name='publisher')
+    shop_xl = pd.read_excel('Dataset.xlsx', sheet_name='shop')
+    book_xl = pd.read_excel('Dataset.xlsx', sheet_name='book')
+    stock_xl = pd.read_excel('Dataset.xlsx', sheet_name='stock')
+    sale_xl = pd.read_excel('Dataset.xlsx', sheet_name='sale')
+    # files = os.listdir()
+    # for file in files:
+    #     if 'sales.csv' in file:
+    #         path_to_file = os.path.join(folder, file)
+    #         with open(path_to_file, 'rt', encoding='utf-8') as f:
+    #             rows = []
+    #             for line in f:
+    #                 rows.append(line.rstrip('\n'))
+
+            # f.close()
+
+    pprint(publisher_xl)
 
 def show_sales():
     pass
 
+
 run = True
+
+
 def show_help():
     print('''
     help    - показать помощь
+    create  - создать таблицы
+    drop    - удалить таблицы и все данные
     load    - загрузить данные из datasets
     sales   - показать продажи по автору
     exit    - выход
     ''')
     return
 
+
 while run:
     i = input('Введите команду (help - помощь): ')
     if i == 'load':
         load_data()
+    elif i == 'create':
+        create_tables(engine, 'create')
+    elif i == 'drop':
+        create_tables(engine, 'drop')
     elif i == 'help':
         show_help()
     elif i == 'sales':
         show_sales()
     elif i == 'exit':
         run = False
-
