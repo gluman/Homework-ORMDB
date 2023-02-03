71b8d13f1ea287d84e04f60b6124addca9caa542
Glumov Andrey <gluman@yandex.ru>
2023-02-03 23:40:32 +0300

    commit 2023.02.03 v1.1

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Подключается к БД любого типа на ваш выбор (например, к PostgreSQL). +
 # Импортирует необходимые модели данных. +
 # Принимает имя или идентификатор издателя (publisher), например через input(). Выводит построчно факты покупки книг этого издателя:
-# import os
-# import pandas as pd
-# import pprint
+
+import os
+import pandas as pd
+import pprint
+import json
 
 import sqlalchemy
 import sqlalchemy as sq
@@ -51,7 +53,7 @@ class Sale(Base):
     __tablename__ = "sale"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer, nullable=False)
+    price = sq.Column(sq.Float, nullable=False)
     date_sale = sq.Column(sq.Date, nullable=False)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
@@ -74,28 +76,53 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-# def load_data():
-#     os.chdir('fixtures')
-#     # folder = os.getcwd()
-#
-#     publisher_xl = pd.read_excel('Dataset.xlsx', sheet_name='publisher')
-#     shop_xl = pd.read_excel('Dataset.xlsx', sheet_name='shop')
-#     book_xl = pd.read_excel('Dataset.xlsx', sheet_name='book')
-#     stock_xl = pd.read_excel('Dataset.xlsx', sheet_name='stock')
-#     sale_xl = pd.read_excel('Dataset.xlsx', sheet_name='sale')
-#
-#     pprint(publisher_xl)
+def load_data_from_json():
+    folder = os.getcwd()
+    os.chdir('fixtures')
+    folder = os.getcwd()
+    with open(folder + '/tests_data.json', 'r') as f:
+        data = json.load(f)
+
+    for record in data:
+        model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale,
+        }[record.get('model')]
+        session.add(model(id=record.get('pk'), **record.get('fields')))
+    session.commit()
 
 
+# def show_sales(publ):
 def show_sales():
-    pass
+
+    # q = session.query(Book.title, Shop.name, sum(Sale.count), Sale.date_sale).join(Stock.sale)\
+    # .join(Book.stock)\
+    # .join(Shop.stock)\
+    # .join(Publisher.book)\
+    # .group_by(Publisher.name)\
+    # .filter(Publisher.name == publ)
+    # for s in q.all():
+    #     print('название книги | название магазина| стоимость покупки | дата покупки')
+    #     pprint(s)
+    q = session.query(Stock)
+    print(q)
+    for s in q.all():
+        print('название книги | название магазина| стоимость покупки | дата покупки')
+        print(s)
+        for i in s:
+            pprint(i.date_sale, i.price)
+
+
 
 def show_help():
     print('''
     help    - показать помощь
     create  - создать таблицы
     drop    - удалить таблицы и все данные
-    load    - загрузить данные из datasets
+    load-json - загрузить данные из datasets
     sales   - показать продажи по автору
     exit    - выход
     ''')
@@ -106,13 +133,19 @@ while run:
     i = input('Введите команду (help - помощь): ')
     if i == 'create':
         create_tables(engine, 'create')
-    # elif i == 'load':
-    #     load_data()
+    elif i == 'do-all':
+        create_tables(engine, 'drop')
+        create_tables(engine, 'create')
+        load_data_from_json()
+    elif i == 'load-json':
+        load_data_from_json()
     elif i == 'drop':
         create_tables(engine, 'drop')
     elif i == 'help':
         show_help()
     elif i == 'sales':
+        publ = input('введите издателя:')
+        # show_sales(publ)
         show_sales()
     elif i == 'exit':
         run = False
